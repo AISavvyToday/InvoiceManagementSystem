d38dc96fd9307c024e7cc8d6a024c9c4796c0eef
muhurijson <muhuri.json@gmail.com>
2020-11-13 14:26:10 +0300

    edited whitenoise

--- invoicemanager/settings.py
+++ invoicemanager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 import django_heroku
 import os
+from whitenoise.django import DjangoWhiteNoise
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -51,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
