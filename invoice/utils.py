2f174633d92babb293f324b46acf73d28e0bf2bb
muhurijson <muhuri.json@gmail.com>
2020-11-17 20:35:48 +0300

    fixing logo in invoice pdf

--- invoice/utils.py
+++ invoice/utils.py
@@ -1,4 +1,4 @@
-from io import BytesIO
+from io import BytesIO, StringIO
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
@@ -9,9 +9,9 @@ def render_to_pdf(template_src, context_dict={}):
 	html  = template.render(context_dict)
 	result = BytesIO()
 	links    = lambda uri, rel: os.path.join(settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, ''))
-	pdf = pisa.pisaDocument(BytesIO(html.encode("utf8")), result, link_callback=links)
+	# pdf = pisa.pisaDocument(BytesIO(html.encode("utf8")), result, link_callback=links)
+	pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")),dest=result, link_callback=links)
 	if not pdf.err:
 		return HttpResponse(result.getvalue(), content_type='application/pdf')
 	return None
 
-# pdf      = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")),dest=result, link_callback=links)
