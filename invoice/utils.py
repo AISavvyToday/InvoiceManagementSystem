3169aadff86dd223dad7cba2752e05ef67f1a1b7
muhurijson <muhuri.json@gmail.com>
2020-11-17 21:02:29 +0300

    fixing logo in invoice pdf

--- invoice/utils.py
+++ invoice/utils.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 from django.conf import settings
+import os
 
 def render_to_pdf(template_src, context_dict={}):
 	template = get_template(template_src)
